71c76bd72144087f46bca6ca8434777ace44defb
JongDeug <jongdeug2021@gmail.com>
2024-05-03 18:52:24 +0900

    jest 간략하게 공부

---
+++ test/unit/products.test.js
@@ -0,0 +1,16 @@
+describe('Calculation', () => {
+    test('two plus two is four', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+    test('two plus two is not five', () => {
+        expect(2 + 2).not.toBe(5);
+    });
+});
+
+const mockFunction = jest.fn();
+mockFunction('hello');
+mockFunction();
+
+expect(mockFunction).toBeCalled();
+expect(mockFunction).toBeCalledWith('o');
